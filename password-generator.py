8da9ec10ba94f72901125cdde9c682bc94e053d7
Nico Gründel <nico.gruendel@web.de>
2017-03-22 20:11:37 +0100

    added reCAPTCHA

--- password-generator.py
+++ password-generator.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import render_template
 from hashlib import *
 from binascii import b2a_base64
+import requests as r
 
 
 app = Flask(__name__)
@@ -17,10 +18,12 @@ def gen_password(site, password, remove_special_chars, max_size):
         s = s.replace("=", "").replace("/", "").replace("+", "")
     return s
 
+def validate_captcha(captcha):
+    return r.post("https://www.google.com/recaptcha/api/siteverify", data={"secret":"xxx", "response":captcha, "remoteip":request.remote_addr}).json()["success"]
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    if request.method == 'POST':
+    if request.method == 'POST' and validate_captcha(request.form["g-recaptcha-response"]):
         return render_template('index.html', password=gen_password(request.form['website'], request.form['password'], "rem" in request.form, int(request.form["length"])))
     else:
         return render_template('index.html')
